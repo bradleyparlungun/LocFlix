5d96c778bba85a25efc6b9362421221f74c4f1d3
Alexander Bruun <39629240+alxbrn@users.noreply.github.com>
2019-06-26 16:12:24 +0200

    Add files via upload

---
+++ LocFlix.Wpf/Objects/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocFlix.Wpf.Objects
+{
+    public class Config
+    {
+        public string ApiKey { get; set; }
+        public string BasePath { get; set; }
+    }
+}
